aedf89f0fe9d3c9f86250cde67dff417b6fb1ee7
Emmanuel <emmanuel.doodleblue@gmail.com>
2025-07-23 12:54:13 +0530

    Till episode 3

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
-let heading = React.createElement("h1", {}, "Hello world from React!");
-let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+let JsxComponent= function (){
+   return (<h1>Namaste React🚀</h1>)
+}
+
+const elem = <span>Inline</span>
+
+const HeadingComponent = () => (
+  <div>
+    <JsxComponent/>
+    <h1>{elem}Namaste React Functional Component🚀</h1>
+  </div>
+);
+let root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<HeadingComponent/>)
 
 
